e7356cdd4e3d3e4c55eb80fc4f416fc6b96b052c
Chisel <chisel@chizography.net>
2011-04-04 18:35:03 +0100

    Replace Root.pm with catalyst.pl default output

--- lib/Cleavages/Controller/Root.pm
+++ lib/Cleavages/Controller/Root.pm
@@ -1,16 +1,14 @@
 package Cleavages::Controller::Root;
-# vim: ts=8 sts=4 et sw=4 sr sta
-use strict;
-use warnings;
-use parent 'Catalyst::Controller';
+use Moose;
+use namespace::autoclean;
 
-use base 'Catalyst::Controller::Validation::DFV';
+BEGIN { extends 'Catalyst::Controller' }
 
 #
 # Sets the actions in this controller to be registered with no prefix
 # so they function identically to actions created in MyApp.pm
 #
-__PACKAGE__->config->{namespace} = '';
+__PACKAGE__->config(namespace => '');
 
 =head1 NAME
 
@@ -22,84 +20,50 @@ Cleavages::Controller::Root - Root Controller for Cleavages
 
 =head1 METHODS
 
-=cut
-
 =head2 index
 
+The root page (/)
+
 =cut
 
-# all controller chain bases should hang off of here
-sub language : Chained('/') PathPart('') CaptureArgs(0) {
-    my ($self, $c, $language) = @_;
-    $c->stash(language => 'en');
-}
-
-sub index : Chained('language') PathPart('') :Args(0) {
+sub index :Path :Args(0) {
     my ( $self, $c ) = @_;
 
-    $c->forward('/cleavage/top_cleavage');
-
-    return;
+    # Hello World
+    $c->response->body( $c->welcome_message );
 }
 
-sub app_root : Chained PathPart('') Args(0) {
-    my ($self, $c) = @_;
-    # redirect to the default action
-    $c->response->redirect( $c->uri_for($c->config->{default_uri}) );
-}
+=head2 default
+
+Standard 404 error page
+
+=cut
 
 sub default :Path {
     my ( $self, $c ) = @_;
-    #$c->response->body( 'Page not found' );
+    $c->response->body( 'Page not found' );
     $c->response->status(404);
-    
-    # TODO error template
-    $c->stash->{template} = 'error/404';
 }
-
-sub render : ActionClass('RenderView') {
-    my ($self, $c) = @_;
-
-    # if we have any error(s) in the stash, automatically show the error page
-    if (defined $c->stash->{error}) {
-        $c->stash->{template} = 'error/simple';
-        $c->log->error( $c->stash->{error}{message} );
-    }
-
-#    if (has_died($c)) {
-#        $c->stash->{template} = 'error/simple';
-#        $c->log->error( @{ $c->stash->{view}{error}{messages} } );
-#    }
-}
-
-sub end : Private {
-    my ($self, $c) = @_;
-
-    # render the page
-    $c->forward('render');
-
-    # fill in any forms
-    $c->forward('refill_form'); # from Catalyst::Controller::Validation::DFV
-}
-
 
 =head2 end
 
 Attempt to render a view, if needed.
 
-=cut 
+=cut
 
-#sub end : ActionClass('RenderView') {}
+sub end : ActionClass('RenderView') {}
 
 =head1 AUTHOR
 
-Chisel Wright
+Chisel,,,
 
 =head1 LICENSE
 
-This library is free software, you can redistribute it and/or modify
+This library is free software. You can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =cut
+
+__PACKAGE__->meta->make_immutable;
 
 1;
