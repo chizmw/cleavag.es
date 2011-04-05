29d94c445a163ddb9227324f6dd042604c2678d7
Chisel <chisel@chizography.net>
2011-04-05 10:48:49 +0100

    Start fixing sign-up process

--- lib/Cleavages/Controller/User.pm
+++ lib/Cleavages/Controller/User.pm
@@ -1,10 +1,49 @@
 package Cleavages::Controller::User;
 use Moose;
+    extends 'Catalyst::Controller::reCAPTCHA',
+            'Catalyst::Controller::Validation::DFV',
+            'Cleavages::ControllerBase::FormValidation';
 use namespace::autoclean;
 
-BEGIN {extends 'Catalyst::Controller'; }
+BEGIN { extends 'Catalyst::Controller'; }
 
-extends 'Catalyst::Controller::reCAPTCHA';
+# form validation modules
+use Cleavages::DFV qw(:constraints);
+use Data::FormValidator::Constraints qw(:closures);
+# form validation data
+our %dfv_profile_for = (
+    login => {
+        required => [qw<
+            username
+            password
+        >],
+
+        filters => 'trim',
+    },
+
+    signup => {
+        required => [qw<
+            new_first_name
+            new_last_name
+            new_username
+            new_email_address
+            new_password
+            new_password_confirm
+        >],
+
+        constraint_methods => {
+            new_email_address => email(),
+
+            new_password_confirm =>
+                constraint_confirm_equal(
+                    { fields => [qw/new_password new_password_confirm/] },
+                    'passwords-do-not-match',
+                ),
+        },
+
+        filters => 'trim',
+    },
+);
 
 sub user_base : Chained('/language') PathPart('user') CaptureArgs(0) {
     # do nothing for now
@@ -16,6 +55,7 @@ sub index :Path :Args(0) {
     $c->response->body('Matched Cleavages::Controller::User in User.');
 }
 
+# display the signup page
 sub signup : Chained('user_base') PathPart('signup') Args(0) {
     my ($self, $c) = @_;
     
@@ -36,6 +76,60 @@ sub signup : Chained('user_base') PathPart('signup') Args(0) {
     return;
 }
 
+# process a signup request
+sub user_signup :Private {
+    my ($self, $c) = @_;
+
+    # check the form for errors
+    $c->forward('form_check', [$dfv_profile_for{signup}]);
+
+    # if the captcha is enabled
+    if ($c->config->{recaptcha}{enabled}) {
+        # check the captcha
+        $c->forward('captcha_check');
+        # deal with any errors
+        if ($c->stash->{recaptcha_error}) {
+            # add to form validation failures
+            $c->forward(
+                'add_form_invalid',
+                [ 'recaptcha', $c->stash->{recaptcha_error} ]
+            );
+        }
+    }
+
+    # check to see if the username has already been used
+    $c->forward('check_unique_username', ['new_username']);
+
+    # if we didn't have any errors, create the new account
+    if ($c->stash->{validation}->success) {
+        # add the new user in a transaction
+        eval {
+            #$c->stash->{schema}->txn_do(
+            my $schema = $c->model('Cleavages::Person')->result_source->schema;
+            $schema->txn_do(
+                sub { $self->_txn_add_user($c) }
+            );
+        };
+        # deal with any errors
+        if ($@) { # Transaction failed
+            die "something terrible has happened!" #
+                if ($@ =~ m{Rollback failed}); # Rollback failed
+
+            # publish the txn error
+            my $error = $@;
+            $error =~ s/(?:called\s+)?at\s+.+?line\s+\d+/__FILEINFO__/xmsg;
+            $c->log->error( $error );
+            $c->stash->{error}{message} =
+                qq{Database transaction failed: $error};
+            return;
+        }
+
+        $c->response->redirect( $c->uri_for($c->config->{default_uri}) );
+        return;
+    }
+
+    return;
+}
 
 __PACKAGE__->meta->make_immutable;
 
